0fd7e22d91089206f25006794fa43c5f574ced7d
rameem2003 <rameem2019@gmail.com>
2022-04-25 10:13:37 +0600

    uodate

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ var next = document.getElementById("next");
 // music list
 const musicList = [
     {
+        name : "স্বপ্ন যাবে বাড়ি আমার",
+        artist : "Grameenphone",
+        src : "./music/shopno_jabe_bari.mp3",
+        imgSrc : "./img/shopno-jabe-bari-amar-grameenphone.jpg"
+    },
+
+    {
         name : "Over The Horizon",
         artist : "Samsung",
         src : "./music/over_the_horizon.mp3",
@@ -17,18 +24,32 @@ const musicList = [
     },
 
     {
-        name : "Bhalobasbo Bashbo Re",
-        artist : "Habib",
+        name : "ভালবাসবো বাসবো রে বন্ধু",
+        artist : "Habib Wahid",
         src : "./music/Bhalobashbo_Bashbo_Re_by_Habib.mp3",
         imgSrc : "./img/bhalo_basbo_basbo_re.jpg"
     },
 
     {
-        name : "Khola Janala",
+        name : "খোলা জানালা",
         artist : "Tahsin Ahmed",
         src : "./music/khola_janala_tahsin_ahmed.mp3",
         imgSrc : "./img/khola_janala_tahsin.jpg"
-    }
+    },
+
+    {
+        name : "এই অবেলায়",
+        artist : "Shironamhin",
+        src : "./music/ei_obelay_shironamhin.mp3",
+        imgSrc : "./img/ei_obelay.jpg"
+    },
+
+    {
+        name : "Tere Liye",
+        artist : "Sreya Ghosal, Atif Aslam",
+        src : "./music/tere_liye.mp3",
+        imgSrc : "./img/tere_liye.jpg"
+    },
 ]
 
 const loadMusic = (musicList) => {
@@ -52,7 +73,7 @@ const pauseMusic = () => {
     playState = false;
     music.pause();
     play.classList.replace("fa-pause", "fa-play");
-    image.classList.remove("anim")
+    image.classList.remove("anim");
 }
 
 play.addEventListener("click", () => {
@@ -82,5 +103,9 @@ const prevMusic = () => {
     playMusic();
 }
 
-next.addEventListener("click", nextMusic)
-prev.addEventListener("click", prevMusic)
+next.addEventListener("click", nextMusic);
+prev.addEventListener("click", prevMusic);
+
+window.onload = () =>{
+    loadMusic(musicList[0]);
+}
